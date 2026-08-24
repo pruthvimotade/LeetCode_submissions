5e97ca6a68ead13d7c92a66d07930daeb17d1a7d
Pruthviraj Motade <152158899+pruthvimotade@users.noreply.github.com>
2026-08-24 23:06:32 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -7,10 +7,11 @@ class Solution {
     }
 
     public int solve(int n) {
-        if(n<3) return n;
+        if (n < 3)
+            return n;
         if (dp[n] != 0)
             return dp[n];
-
         return dp[n] = solve(n - 1) + solve(n - 2);
     }
+
 }
